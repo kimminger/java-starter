85462d691985a8477965a4d1cd55f524b49785d5
IsNull <p.buettiker@elderbyte.com>
2018-09-11 11:11:32 +0200

    improved rest exception http status translation

--- starter-spring-cloud/src/main/java/com/elderbyte/spring/cloud/bootstrap/support/RestGenericExceptionHandler.java
+++ starter-spring-cloud/src/main/java/com/elderbyte/spring/cloud/bootstrap/support/RestGenericExceptionHandler.java
@@ -2,6 +2,7 @@ package com.elderbyte.spring.cloud.bootstrap.support;
 
 import com.elderbyte.commons.exceptions.ExceptionUtil;
 import com.elderbyte.commons.exceptions.NotFoundException;
+import feign.FeignException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 
 
 /**
@@ -41,6 +43,34 @@ public class RestGenericExceptionHandler {
         reponse.sendError(HttpStatus.NOT_FOUND.value(), agrMessages);
     }
 
+    @ExceptionHandler(value = {SocketTimeoutException.class})
+    public void timeoutException(
+            HttpServletRequest req,
+            HttpServletResponse reponse,
+            Exception exception) throws IOException {
+
+        String agrMessages = ExceptionUtil.aggregateMessages(exception);
+
+        var message = exception.getClass().getSimpleName() + ": " + agrMessages + " resource: " +  req.getRequestURL();
+
+        logger.error(message, exception);
+
+        reponse.sendError(HttpStatus.GATEWAY_TIMEOUT.value(), HttpStatus.GATEWAY_TIMEOUT.value() + " - " + message);
+    }
+
+
+    @ExceptionHandler(value = {FeignException.class})
+    public void gatewayException(
+            HttpServletRequest req,
+            HttpServletResponse reponse,
+            Exception exception) throws IOException {
+
+        var message = exception.getClass().getSimpleName() + ": " + ExceptionUtil.aggregateMessages(exception) + " resource: " +  req.getRequestURL();
+
+        logger.error(message, exception);
+
+        reponse.sendError(HttpStatus.BAD_GATEWAY.value(), HttpStatus.BAD_GATEWAY.value() + " - " + message);
+    }
 
     /**
      * Generic Exception handler translates them to INTERNAL_SERVER_ERROR Http codes
@@ -53,10 +83,11 @@ public class RestGenericExceptionHandler {
 
         String agrMessages = ExceptionUtil.aggregateMessages(exception);
 
-        logger.error("Unhandled Exception: " + agrMessages + " resource: " +  req.getRequestURL(), exception);
+        var message = exception.getClass().getSimpleName() + ": " + agrMessages + " resource: " +  req.getRequestURL();
 
-        reponse.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.value() + " - Server Error: " + agrMessages);
+        logger.error(message, exception);
+
+        reponse.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.value() + " - " + message);
     }
-
 
 }
