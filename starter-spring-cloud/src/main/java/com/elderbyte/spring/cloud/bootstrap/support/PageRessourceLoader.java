5daaf42a298d3041c911358235e7f2a108300e39
n0daft <home@ericneher.ch>
2018-09-14 11:51:40 +0200

    added pageRessourceLoader

---
+++ starter-spring-cloud/src/main/java/com/elderbyte/spring/cloud/bootstrap/support/PageRessourceLoader.java
@@ -0,0 +1,34 @@
+package com.elderbyte.spring.cloud.bootstrap.support;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class PageRessourceLoader {
+
+    private static final Logger logger = LoggerFactory.getLogger(PageRessourceLoader.class);
+
+    public static <T> List<T> loadPages(int pageSize, Function<Pageable, Page<T>>
+            pageLoader){
+
+        Page<T> page = pageLoader.apply(PageRequest.of(0, pageSize));
+        List<T> totalResult = new ArrayList<>(page.getContent());
+
+        while(page.hasNext()) {
+
+            page = pageLoader.apply(page.nextPageable());
+            totalResult.addAll(page.getContent());
+        }
+
+        return totalResult;
+
+    }
+
+
+}
