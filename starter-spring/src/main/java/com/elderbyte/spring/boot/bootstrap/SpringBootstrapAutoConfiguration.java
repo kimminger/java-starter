8635f1b36e966faa7a197276817f3538faa45c2c
Kim Rohner <kr@acs-ag.ch>
2019-08-26 11:56:25 +0200

    Automatically pickup MicrometerConfiguration

--- starter-spring/src/main/java/com/elderbyte/spring/boot/bootstrap/SpringBootstrapAutoConfiguration.java
+++ starter-spring/src/main/java/com/elderbyte/spring/boot/bootstrap/SpringBootstrapAutoConfiguration.java
@@ -2,13 +2,15 @@ package com.elderbyte.spring.boot.bootstrap;
 
 
 import com.elderbyte.spring.boot.bootstrap.data.DataConfiguration;
+import com.elderbyte.spring.boot.bootstrap.metrics.MicrometerConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-        DataConfiguration.class
-        })
+        DataConfiguration.class,
+        MicrometerConfiguration.class
+})
 public class SpringBootstrapAutoConfiguration {
 
 
